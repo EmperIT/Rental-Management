14d3854ab62a3c2c26b527460306f7a3e59df83c
Emper <minhtruongb16@gmail.com>
2025-04-24 20:21:27 +0700

    Update Add Contract

--- frontend/src/components/contract/HeaderContract.js
+++ frontend/src/components/contract/HeaderContract.js
@@ -1,12 +1,20 @@
+import React, { useState } from 'react';
 import "../../styles/contract/HeaderContract.css";
+import CreateContractModal from "./CreateContractModal";
 function Header() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <div className="header-contract">
-      <h1 className="dashboard-title">Quản lý hợp đồng</h1>
-      <button className="btn btn-primary">
-        Thêm hợp đồng
-        </button>  
-    </div>
+    <>
+      <div className="header-contract">
+        <h1 className="dashboard-title">Quản lý hợp đồng</h1>
+        <button className="btn btn-primary" onClick={() => setOpenModal(true)}>
+          Thêm hợp đồng
+        </button>
+      </div>
+      <CreateContractModal isOpen={openModal} onClose={() => setOpenModal(false)} />
+    </>
   );
 }
+
 export default Header;
