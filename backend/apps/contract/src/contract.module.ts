5f844c868dc8564326637823e6161bcd2aa31c27
Emper <minhtruongb16@gmail.com>
2025-04-24 20:22:43 +0700

    Update Add Contract

--- backend/apps/contract/src/contract.module.ts
+++ backend/apps/contract/src/contract.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ContractSchema } from './schemas/contract.schema';
+import { ContractTemplateSchema } from './schemas/contract.schema'; // ✅ Thêm dòng này
 import { ContractService } from './contract.service';
 import { ContractController } from './contract.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -8,7 +9,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forFeature([{ name: 'Contract', schema: ContractSchema }]),
+    MongooseModule.forFeature([
+      { name: 'Contract', schema: ContractSchema },
+      { name: 'ContractTemplate', schema: ContractTemplateSchema },
+    ]),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => {
